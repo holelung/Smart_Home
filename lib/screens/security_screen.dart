4da3fb9b06bb9b7ed534ab198884e4b3b60d238c
holelung <junho335276@gmail.com>
2024-06-04 18:37:30 +0900

    Security 모바일화면 버그수정

--- lib/screens/security_screen.dart
+++ lib/screens/security_screen.dart
@@ -1,47 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:smart_home_project/widgets/security_button_widget.dart';
 
-class SecurityScreen extends StatelessWidget {
+class SecurityScreen extends StatefulWidget {
   const SecurityScreen({super.key});
+
+  @override
+  State<SecurityScreen> createState() => _SecurityScreenState();
+}
+
+class _SecurityScreenState extends State<SecurityScreen> {
+  var isMobile = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    isMobile = MediaQuery.of(context).size.width < 600;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20),
-      child: Row(
-        children: [
-          Image.network(
-            'https://st.depositphotos.com/2274151/3518/i/450/depositphotos_35186549-stock-photo-sample-grunge-red-round-stamp.jpg',
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-          const Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
+      child: isMobile
+          ? Column(
               children: [
-                SecurityButton(
-                  text: '통화',
-                  icon: Icons.call,
+                Image.network(
+                  'https://st.depositphotos.com/2274151/3518/i/450/depositphotos_35186549-stock-photo-sample-grunge-red-round-stamp.jpg',
                 ),
-                SecurityButton(
-                  text: '열림',
-                  icon: Icons.lock_open_rounded,
+                const SizedBox(
+                  width: 20,
                 ),
-                SecurityButton(
-                  text: '방범',
-                  icon: Icons.sensor_occupied_rounded,
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SecurityButton(
+                        text: '통화',
+                        icon: Icons.call,
+                      ),
+                      SecurityButton(
+                        text: '열림',
+                        icon: Icons.lock_open_rounded,
+                      ),
+                      SecurityButton(
+                        text: '방범',
+                        icon: Icons.sensor_occupied_rounded,
+                      ),
+                      SecurityButton(
+                        text: '기록',
+                        icon: Icons.folder_open_rounded,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          : Row(
+              children: [
+                Image.network(
+                  'https://st.depositphotos.com/2274151/3518/i/450/depositphotos_35186549-stock-photo-sample-grunge-red-round-stamp.jpg',
                 ),
-                SecurityButton(
-                  text: '기록',
-                  icon: Icons.folder_open_rounded,
+                const SizedBox(
+                  width: 20,
                 ),
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SecurityButton(
+                        text: '통화',
+                        icon: Icons.call,
+                      ),
+                      SecurityButton(
+                        text: '열림',
+                        icon: Icons.lock_open_rounded,
+                      ),
+                      SecurityButton(
+                        text: '방범',
+                        icon: Icons.sensor_occupied_rounded,
+                      ),
+                      SecurityButton(
+                        text: '기록',
+                        icon: Icons.folder_open_rounded,
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
